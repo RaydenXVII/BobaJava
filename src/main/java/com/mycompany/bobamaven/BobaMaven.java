e4c23e8f26a8bdc4ca642d877bdd2aa7b5764627
RaydenXVII <wantedgamer03@gmail.com>
2023-07-20 21:08:16 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/bobamaven/BobaMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bobamaven;
+
+/**
+ *
+ * @author ACER
+ */
+public class BobaMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
